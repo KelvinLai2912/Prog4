d93cbfb950640877979f6d56b91bcc64bb8027f9
KelvinLai2912 <kelvin.lai@live.nl>
2023-05-20 01:39:27 +0200

    login gives token

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -65,7 +65,7 @@ module.exports = {
             }
           }
         });
-        pool.releaseConnection(connection);
+        connection.release();
       }
     });
   },
--- src/util/utils.js
+++ src/util/utils.js
@@ -6,5 +6,7 @@ module.exports = {
     preprocess: function (data) {
       data.title = data.title.toUpperCase();
     }
-  })
+  }),
+
+jwtSecretKey: process.env.JWT_SECRET || '87R2hj3vhr38vHR673vH3vhR673Vhr3V8H67v3FV6'
 };
